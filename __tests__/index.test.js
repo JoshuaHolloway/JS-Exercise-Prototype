82a6d656efc76695af91246e58838d2df6290a62
JoshuaHolloway <jhollow6@asu.edu>
2021-03-17 01:08:05 -0500

    task 1

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,10 +1,10 @@
 import functions from '../index';
 
-describe('fooFunction', ()=>{
-    it('foo returns foo', ()=>{
-        expect(functions.foo()).toBe('bar');
-    })
-});
+// describe('fooFunction', ()=>{
+//     it('foo returns foo', ()=>{
+//         expect(functions.foo()).toBe('bar');
+//     })
+// });
 
 describe('Instances of Person', () => {
     let neo
@@ -46,61 +46,61 @@ describe('Instances of Person', () => {
       expect(str).toContain('Neo')
       expect(str).toContain(20)
     })
-  })
-  
-  //car
-  describe('Instances of Car', () => {
-    let batmobile
-    beforeEach(() => {
-      batmobile = new functions.Car('BatMobile', 20)
-    })
-    it('initialize with the given model', () => {
-      expect(batmobile.model).toBe('BatMobile')
-    })
-    it('initialize with the given milesPerGallon', () => {
-      expect(batmobile.milesPerGallon).toEqual(20)
-    })
-    it('initialize with an empty tank', () => {
-      expect(batmobile.tank).toEqual(0)
-    })
-    it('initialize with an odometer at 0 miles', () => {
-      expect(batmobile.odometer).toEqual(0)
-    })
-    it('get fill methods from their prototype', () => {
-      expect(batmobile.__proto__.fill).not.toBeUndefined();
-    })
-    it('fill method increases the tank by the given gallons', () => {
-      batmobile.fill(10)
-      expect(batmobile.tank).toEqual(10)
-      batmobile.fill(10)
-      expect(batmobile.tank).toEqual(20)
-    })
-  })
-  
-  describe('Instances of Baby', () => {
-    let baby
-    beforeEach(() => {
-      baby = new functions.Baby('Lucy', 5, 'trains')
-    })
-    it('initialize with the given name', () => {
-      expect(baby.name).toBe('Lucy')
-    })
-    it('initialize with the given age', () => {
-      expect(baby.age).toEqual(5)
-    })
-    it('initialize with the given favorite toy', () => {
-      expect(baby.favoriteToy).toBe('trains')
-    })
-    it('get a play method from their prototype', () => {
-      expect(baby.__proto__.play).not.toBeUndefined();
-    })
-    it('can play with favorite toy', () => {
-      expect(baby.play()).toContain('trains')
-    })
-    it('inherit the methods on Person.prototype', () => {
-      expect(baby.__proto__.eat).not.toBeUndefined();
-    })
-    it('inherit the methods on Person.prototype', () => {
-        expect(baby.__proto__.poop).not.toBeUndefined();
-      })
-  })
+})
+
+//car
+// describe('Instances of Car', () => {
+//   let batmobile
+//   beforeEach(() => {
+//     batmobile = new functions.Car('BatMobile', 20)
+//   })
+//   it('initialize with the given model', () => {
+//     expect(batmobile.model).toBe('BatMobile')
+//   })
+//   it('initialize with the given milesPerGallon', () => {
+//     expect(batmobile.milesPerGallon).toEqual(20)
+//   })
+//   it('initialize with an empty tank', () => {
+//     expect(batmobile.tank).toEqual(0)
+//   })
+//   it('initialize with an odometer at 0 miles', () => {
+//     expect(batmobile.odometer).toEqual(0)
+//   })
+//   it('get fill methods from their prototype', () => {
+//     expect(batmobile.__proto__.fill).not.toBeUndefined();
+//   })
+//   it('fill method increases the tank by the given gallons', () => {
+//     batmobile.fill(10)
+//     expect(batmobile.tank).toEqual(10)
+//     batmobile.fill(10)
+//     expect(batmobile.tank).toEqual(20)
+//   })
+// })
+
+// describe('Instances of Baby', () => {
+//   let baby
+//   beforeEach(() => {
+//     baby = new functions.Baby('Lucy', 5, 'trains')
+//   })
+//   it('initialize with the given name', () => {
+//     expect(baby.name).toBe('Lucy')
+//   })
+//   it('initialize with the given age', () => {
+//     expect(baby.age).toEqual(5)
+//   })
+//   it('initialize with the given favorite toy', () => {
+//     expect(baby.favoriteToy).toBe('trains')
+//   })
+//   it('get a play method from their prototype', () => {
+//     expect(baby.__proto__.play).not.toBeUndefined();
+//   })
+//   it('can play with favorite toy', () => {
+//     expect(baby.play()).toContain('trains')
+//   })
+//   it('inherit the methods on Person.prototype', () => {
+//     expect(baby.__proto__.eat).not.toBeUndefined();
+//   })
+//   it('inherit the methods on Person.prototype', () => {
+//       expect(baby.__proto__.poop).not.toBeUndefined();
+//     })
+// })
